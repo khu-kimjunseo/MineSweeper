e4d15787c4accd0168b5c9546d51f28b84f76032
kimjunseo <juny7s6@khu.ac.kr>
2022-09-28 12:56:54 +0900

    Update I/O handler

--- Game.py
+++ Game.py
@@ -5,6 +5,7 @@ import random
 
 class Minesweeper(tk.Frame):
     element = {"MINE":0, "CLICK":1}
+    click = {"LEFT":-1, "NON":0, "RIGHT":1}
 
     def __init__(self, master):
         super(Minesweeper, self).__init__(master)
@@ -23,6 +24,9 @@ class Minesweeper(tk.Frame):
         self.canvas.bind('<Button-2>', self.right_click)  
 
     def setup_game(self, row, col):
+        # Cheat code
+        self.canvas.delete('bomb')
+
         self.row = row
         self.col = col
         self.width = self.col * self.square
@@ -37,29 +41,55 @@ class Minesweeper(tk.Frame):
         for i in range(self.row+1):
             self.canvas.create_line(0,i*self.square,self.width,i*self.square,fill='black')
 
-        self.pattern = np.arange(self.col * self.row * 3).reshape(self.col, self.row, 3)
+        self.pattern = np.zeros(self.col * self.row * 3, dtype = 'int32').reshape(self.col, self.row, 3)
 
-        
+        # Create mine
+        for _ in range(self.mine):
+            i = random.randint(0, self.col-1)
+            j = random.randint(0, self.row-1)
+            while self.pattern[i][j][Minesweeper.element["MINE"]] == -1:       
+                i = random.randint(0, self.col-1)
+                j = random.randint(0, self.row-1)
+            
+            self.pattern[i][j][Minesweeper.element["MINE"]] = -1  
+            # Cheat code
+            self.canvas.create_text((i+0.5)*self.square, (j+0.5)*self.square, text = 'O', fill = 'black', font = ('Helvetica', 40), tags = 'bomb')
     
     def setup_9x9(self):
-        self.setup_game(9, 9)
         self.mine = 10
+        self.setup_game(9, 9)
 
     def setup_16x16(self):
-        self.setup_game(16, 16)
         self.mine = 40
+        self.setup_game(16, 16)
 
     def setup_30x16(self):
-        self.setup_game(16, 30)
         self.mine = 99
+        self.setup_game(16, 30)
 
     def left_click(self, event):
         x = event.x//self.square
         y = event.y//self.square
+        if self.pattern[x][y][Minesweeper.element["MINE"]] == -1:
+            messagebox.showinfo('BOMB', 'BOMB!')
+            self.setup_game(self.row, self.col)
+        else:
+            self.detect_region(x, y)                
 
     def right_click(self, event):  
         x = event.x//self.square
         y = event.y//self.square
+        if self.pattern[x][y][Minesweeper.element["CLICK"]] == Minesweeper.click["NON"]:
+            self.canvas.create_text((x+0.5)*self.square, (y+0.5)*self.square, text = 'X', fill = 'red', font = ('Helvetica', 40), tags = '[{},{}]'.format(x,y))
+            self.pattern[x][y][Minesweeper.element["CLICK"]] = Minesweeper.click["RIGHT"]
+        elif self.pattern[x][y][Minesweeper.element["CLICK"]] == Minesweeper.click["RIGHT"]:
+            self.canvas.delete('[{},{}]'.format(x,y))
+            self.pattern[x][y][Minesweeper.element["CLICK"]] = Minesweeper.click["NON"]
+        
+    def detect_region(self, x, y):
+        pass
+        
+            
 
 
 if __name__ == '__main__':
